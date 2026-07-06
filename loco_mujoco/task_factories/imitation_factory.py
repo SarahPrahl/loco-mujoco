f555c38eebedf4f7434fea571e2918f4120a88ab
p756017 <sarah.prahl@tu-dortmund.de>
2026-07-06 19:32:13 +0200

    Added option to use all files of a directory for the custom trajectory

--- loco_mujoco/task_factories/imitation_factory.py
+++ loco_mujoco/task_factories/imitation_factory.py
@@ -120,10 +120,6 @@ class ImitationFactory(TaskFactory):
         env_name = env.__class__.__name__
         if "Mjx" in env_name:
             env_name = env_name.replace("Mjx", "")
-        if "Inspire" in env_name:
-            env_name = env_name.replace("Inspire", "")
-        if "Ball" in env_name:
-            env_name = env_name.replace("Ball", "")
 
         if isinstance(default_dataset_conf.task, str):
             default_dataset_conf.task = [default_dataset_conf.task]
@@ -255,10 +251,6 @@ class ImitationFactory(TaskFactory):
                 else [lafan1_dataset_conf.dataset_name]
             
         env_name = env.__class__.__name__
-        if "Inspire" in env_name:
-            env_name = env_name.replace("Inspire", "")
-        if "Ball" in env_name:
-            env_name = env_name.replace("Ball", "")
 
         # Load LAFAN1 Trajectory
         traj = load_lafan1_trajectory(env_name, dataset_paths)
@@ -285,14 +277,22 @@ class ImitationFactory(TaskFactory):
         env_name = env.__class__.__name__
         if "Mjx" in env_name:
             env_name = env_name.replace("Mjx", "")
-        if "Inspire" in env_name:
-            env_name = env_name.replace("Inspire", "")
-        if "Ball" in env_name:
-            env_name = env_name.replace("Ball", "")
+
+        file_paths = []
+        
+        if custom_dataset_conf.paths is not None:
+            file_paths = custom_dataset_conf.paths
+        if custom_dataset_conf.directory is not None:
+            if os.path.isdir(custom_dataset_conf.directory):
+                for file in os.listdir(custom_dataset_conf.directory):
+                    if file.endswith(".npz"):
+                        file_paths.append(os.path.join(custom_dataset_conf.directory, file))
+            else:
+                raise ValueError(f"Invalid directory path: {custom_dataset_conf.directory}")
         
         trajs = []
         # load each trajectory from the provided paths and concatenate them
-        for file_path in custom_dataset_conf.paths:
+        for file_path in file_paths:
 
             traj = Trajectory.load(file_path)
 
